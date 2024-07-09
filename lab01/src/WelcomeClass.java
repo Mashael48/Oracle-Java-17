d566c48a08a2565d0dfc4abe977cdd226d24eb21
Mashael-Sh <Masahael.Shenaiber@gmail.com>
2024-07-09 09:55:34 +0300

    Number abstract class

--- lab01/src/WelcomeClass.java
+++ lab01/src/WelcomeClass.java
@@ -1,49 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class WelcomeClass {
-    public static void main(String[] args) {
-        System.out.println("\nHello :) \n");
+	public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 7, 0};
+		Number[] numbers = new Number[4];
 
-        printArray(array);
-        swap(1, 2);
-        printArray(array);
+		numbers[0] = 10;
+		numbers[1] = 15.344;
+		numbers[2] = new BigInteger("8");
+		numbers[3] = new BigDecimal("8.1");
 
-        swap(array, 1, 2);
-        printArray(array);
+		System.out.println("Largers: " + getLargest(numbers));
+	}
 
-        Arrays.sort(array);
-        printArray(array);
+	private static Number getLargest(Number[] numbers) {
+		Number number = numbers[0];
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(3);
-        list.add(1, 2);
-        System.out.println(list);
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i].doubleValue() > number.doubleValue()) {
+				number = numbers[i];
+			}
+		}
 
-    }
-
-    private static void swap(int a, int b) {
-        int temp = b;
-        b = a;
-        a = temp;
-    }
-
-    private static void swap(int[] array, int a, int b) {
-        int temp = array[b];
-        array[b] = array[a];
-        array[a] = temp;
-
-    }
-
-    private static void printArray(int[] array) {
-//		for (int i = 0; i < array.length; i++) {
-//			System.out.println(array[i]);
-//		}
-//      System.out.println("-------------------");
-
-//        System.out.println(Arrays.toString(array));
-    }
+		return number;
+	}
 }
